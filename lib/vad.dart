df0a93defa558cfc34a6956ac2d15c08304bded6
Sang Doan <sang@play.ht>
2025-03-13 14:45:50 -0700

    remove web export

--- lib/vad.dart
+++ lib/vad.dart
@@ -6,5 +6,4 @@ export 'src/audio_streamer.dart';
 export 'src/vad_event.dart';
 export 'src/vad_handler_base.dart';
 export 'src/vad_handler_non_web.dart' show VadHandlerNonWeb;
-export 'src/vad_handler_web.dart' show VadHandlerWeb;
 export 'src/vad_iterator.dart';
